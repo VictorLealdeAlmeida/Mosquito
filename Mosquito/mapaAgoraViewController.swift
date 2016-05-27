0c8a44b4a92c7a44e5d5124b308e23308e4ce291
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor.local>
2016-05-27 18:51:22 -0300

    Mapa rodando com marcacoes

--- Mosquito/mapaAgoraViewController.swift
+++ Mosquito/mapaAgoraViewController.swift
@@ -9,13 +9,44 @@
 import Foundation
 
 import UIKit
+import MapKit
 
-class mapaAgoraViewController: UIViewController {
+class mapaAgoraViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapa: MKMapView!
+    
+    class Lugar:  NSObject, MKAnnotation {
+        var title: String?
+        var subtitle: String?
+        var coordinate: CLLocationCoordinate2D
+        init(title:String , subtitle:String ,coordinate: CLLocationCoordinate2D) {
+            self.title = title
+            self.subtitle = subtitle
+            self.coordinate = coordinate
+    } }
     
     override func viewDidLoad() {
+ 
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapa.delegate = self
+        let startCoord = CLLocationCoordinate2DMake(-8.0526143,-34.8801386);
+        let adjustedRegion = mapa.regionThatFits(MKCoordinateRegionMakeWithDistance(startCoord, 1000, 1000))
+        mapa.setRegion(adjustedRegion, animated: true)
+        mapa.addAnnotation(Lugar(title:"Jump Brasil", subtitle: "Aceleradora", coordinate:startCoord))
+        
+        let cac = MKPointAnnotation()
+        cac.title = "CAC"
+        cac.subtitle = "UFPE"
+        cac.coordinate = CLLocationCoordinate2DMake(-8.050594, -34.953424)
+        mapa.addAnnotation(cac)
+        
+        
+       
     }
+    
+    
+    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
