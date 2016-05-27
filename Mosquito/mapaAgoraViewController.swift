58e3957295c8e6582fbe1b14ae3290a4f12764e5
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor.local>
2016-05-27 17:36:54 -0300

    Primeiro commit

---
+++ Mosquito/mapaAgoraViewController.swift
@@ -0,0 +1,26 @@
+//
+//  mapaAgoraViewController.swift
+//  Mosquito
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 27/05/16.
+//  Copyright © 2016 Victor Leal Porto de Almeida Arruda. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class mapaAgoraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
