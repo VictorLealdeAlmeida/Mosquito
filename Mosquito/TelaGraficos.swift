f082aaa49ba4a82f8a03b55c59757038e49e6954
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor.local>
2016-05-27 23:37:35 -0300

    Raio proporcional

--- Mosquito/TelaGraficos.swift
+++ Mosquito/TelaGraficos.swift
@@ -17,15 +17,20 @@ class TelaGraficosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        barraTermometro.x = 0
+        self.barraTermometro.alpha = 0
+        UIView.animateWithDuration(1) {
+            self.barraTermometro.alpha = 1
+        }
         
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     
 }
